3bf25951ccbdd1b3fe11ac2beef3dc1302589b17
zhaolong <zhaolong@icbench.com>
2021-07-22 15:22:07 +0800

    a small error now repaired

--- rtl/sha256/top.v
+++ rtl/sha256/top.v
@@ -1,4 +1,4 @@
-/ DESCRIPTION: Verilator: Verilog example module
+//DESCRIPTION: Verilator: Verilog example module
 //
 // This file ONLY is placed under the Creative Commons Public Domain, for
 // any use, without warranty, 2003 by Wilson Snyder.
